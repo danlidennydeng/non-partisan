f37a16096f2b313983c0daf190946d52f6ceb1d8
Danli Denny Deng <danlideng@yahoo.com>
2023-05-10 11:27:19 -0700

    whoweare test for mobile

---
+++ Components/whoweare.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { motion } from "framer-motion";
+import styles from '../styles/Home.module.css';
+
+function Whoweare() {
+
+
+  return (
+    
+
+   <div className={styles.whoweareBox}>
+    
+    
+   </div>
+    
+  )
+}  
+
+export default Whoweare
--- pages/index2.js
+++ pages/index2.js
@@ -1,22 +1,17 @@
 import Logo from '../Components/Logo'
 import Snake from '../Components/Snake'
 import Benjamin from '../Components/benjamin'
+import Whoweare from '../Components/whoweare'
 
 export default function Index2() {
   return (
     <>
     <div>
-    <div>
-    
-      <Snake />
+      <div><Snake /></div>
+      <div><Whoweare /></div>
+      <div><Benjamin /></div>
       
-      </div>
-      
-      <div>
-        <Benjamin />
-      </div>
-
-      </div>
+    </div>
       </>
   )
 }
