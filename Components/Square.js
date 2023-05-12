d93fb0ff827070bf32e6b46bf4bf490058d3cef1
Danli Denny Deng <danlideng@yahoo.com>
2023-05-12 10:45:58 -0700

    2 squares layout test

---
+++ Components/Square.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import { motion } from "framer-motion";
+import styles from '../styles/Home.module.css';
+
+function Square() {
+
+
+  return (
+    
+
+   <div className={styles.whoweareBox}>
+    <div className={styles.square1}>Non-Partisan Alliance Inc. is a non-profit organization registered in the state of Delaware, and operates exclusively within the U.S.A.</div>
+    <div className={styles.square2}>We are establishing a clean and professional online political discussion forum...</div>
+    
+   </div>
+    
+  )
+}  
+
+export default Square
--- pages/index2.js
+++ pages/index2.js
@@ -2,12 +2,14 @@ import Logo from '../Components/Logo'
 import Snake from '../Components/Snake'
 import Benjamin from '../Components/benjamin'
 import Whoweare from '../Components/whoweare'
+import Square from '../Components/Square'
 
 export default function Index2() {
   return (
     <>
     <div>
       <div><Snake /></div>
+      <div><Square /></div>
       <div><Whoweare /></div>
       <div><Benjamin /></div>
       
